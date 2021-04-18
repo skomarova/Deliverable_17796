fd94a0ea460f8acbfb681619ef0c035071c4f8cf
skomarova <komarovs@sheridancollege.ca>
2021-04-18 19:41:58 -0400

    #6 - modified class Game

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -8,17 +8,18 @@ package ca.sheridancollege.project;
 import java.util.ArrayList;
 
 /**
- * The class that models your game. You should create a more specific
- * child of this class and instantiate the methods given.
+ * The class that models your game. You should create a more specific child of this class and instantiate the methods
+ * given.
+ *
  * @author dancye, 2018
+ * @modified by Svetlana Komarova, April 18, 2021
  */
-public abstract class Game 
-{
+public abstract class Game {
+
     private final String gameName;//the title of the game
-    private ArrayList <Player> players;// the players of the game
-    
-    public Game(String givenName)
-    {
+    private ArrayList<Player> players;// the players of the game
+
+    public Game(String givenName) {
         gameName = givenName;
         players = new ArrayList();
     }
@@ -26,39 +27,34 @@ public abstract class Game
     /**
      * @return the gameName
      */
-    public String getGameName() 
-    {
+    public String getGameName() {
         return gameName;
     }
-    
-     /**
+
+    /**
      * @return the players of this game
      */
-    public ArrayList <Player> getPlayers() 
-    {
+    public ArrayList<Player> getPlayers() {
         return players;
     }
 
     /**
      * @param players the players of this game
      */
-    public void setPlayers(ArrayList <Player> players) 
-    {
+    public void setPlayers(ArrayList<Player> players) {
         this.players = players;
     }
-    
+
     /**
-     * Play the game. This might be one method or many method calls depending
-     * on your game.
+     * Play the game. This might be one method or many method calls depending on your game.
      */
     public abstract void play();
-    
-    /**
-     * When the game is over, use this method to declare and display a winning
-     * player.
-     */
-    public abstract void declareWinner();
 
-   
-    
+    /**
+     * When the game is over, use this method to declare and display a winning player.
+     *
+     * @param winner
+     */
+    public abstract void declareWinner(int winner, int lost);
+
 }//end class
