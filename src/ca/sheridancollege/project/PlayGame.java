dddad74c952a83a1464290474096a67d48f6895c
skomarova <komarovs@sheridancollege.ca>
2021-04-19 19:50:00 -0400

    #12 - main class PlayGame added

---
+++ src/ca/sheridancollege/project/PlayGame.java
@@ -0,0 +1,51 @@
+/*
+    Name: Svetlana Komarova
+    Assignment:Deliverable
+    Program: Computer Programmer
+    Date: April 19, 2021
+
+    Description:
+    This program simulates “War”, a two-person game that uses a deck of playing cards.
+*/
+
+package ca.sheridancollege.project;
+
+import java.util.Scanner;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Svetlana Komarova
+ */
+public class PlayGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        
+        //TODO - players 
+//        System.out.print("Enter a name for Player1:");
+//        String name1 = in.nextLine();
+//        System.out.print("Enter a name for Player2:");
+//        String name2 = in.nextLine();
+
+//        WarPlayer p1 = new WarPlayer(name1);
+//        WarPlayer p2 = new WarPlayer(name2);
+        
+        
+        System.out.print("Enter a game name:");
+        String name = in.nextLine();
+        
+        System.out.println("Play the '" + name + "'!\n");
+        
+
+        WarGame myGame = new WarGame(name);
+        
+        myGame.play();
+        
+    }
+
+}
